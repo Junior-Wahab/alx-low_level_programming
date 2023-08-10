eb407ba2e4155575fdc9190cf75e8f23661d6f74
Junior-Wahab <jnrmwahab@gmail.com>
2023-08-10 12:30:22 -0700

    Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Program that prints a line with put function
+ * main - Entry point
+ *
  * Return: 0 
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
