265bd89cdd774fc8a2bff6af9450abd3bfb8aefa
Junior-Wahab <jnrmwahab@gmail.com>
2023-08-15 14:11:03 -0700

    Task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,4 @@
-#include "main.h"
-
-
-	/**
+/**
 	 * print_sign - prints the sign of a number
 	 * @n: the int to check
 	 * Return: 1 and prints + if n is greater than zero
@@ -24,4 +21,3 @@
 		}
 			return (-1);
 	}
-
