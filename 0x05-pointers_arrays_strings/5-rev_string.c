a884f5975bc0694eef8c7691312efd41adf0d60f
Junior-Wahab <jnrmwahab@gmail.com>
2023-08-21 13:15:56 -0700

    Task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
-* print_rev - prints a string in reverse
-* @s: string to print
+*rev_string - Reverse a string
+*@s: String to reverse
+*Return: Nothing
 */
-
-void print_rev(char *s)
+void rev_string(char *s)
 {
-int len = 0, index = 0;
-len = _strlen(s);
-
-for (index = len - 1; index >= 0; index--)
-_putchar(s[index]);
-
-_putchar('\n');
+int i = 0, length;
+length = _strlen(s) - 1;
+while (length > i)
+{
+swap_char(s + length, s + i);
+i++;
+length--;
+}
 }
 
 /**
@@ -28,4 +29,18 @@ for (count = 0; s[count] != '\0'; count++)
 inc++;
 
 return (inc);
+}
+
+/**
+*swap_char - swap two characters
+*@a: first character
+*@b: second character
+*Return: nothing
+*/
+
+void swap_char(char *a, char *b)
+{
+char tmp = *a;
+*a = *b;
+*b = tmp;
 }
